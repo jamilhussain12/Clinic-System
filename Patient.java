d03bb027d4c077a4610c1edd223ec78da2e6ebbb
jamilhussain12 <ajamilhussain9@gmail.com>
2025-04-08 01:26:13 +0500

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,13 @@
+public class Patient {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Patient(int id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
