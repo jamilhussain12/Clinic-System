c49a902deed123672f50b28108de4a3f386bee69
jamilhussain12 <ajamilhussain9@gmail.com>
2025-04-08 01:28:36 +0500

    Create AppointmentController.java

---
+++ AppointmentController.java
@@ -0,0 +1,12 @@
+import model.*;
+import java.util.*;
+
+public class AppointmentController {
+    private List<AppointmentBooking> bookings;
+    private List<Patient> patients;
+ 
+    public AppointmentController() {
+        bookings = new ArrayList<>();
+        patients = new ArrayList<>();
+    }
+}
