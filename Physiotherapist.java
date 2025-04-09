9be40b18bc020017db9d4ed047349b3ba072fd69
jamilhussain12 <ajamilhussain9@gmail.com>
2025-04-10 00:23:54 +0500

    Update Physiotherapist.java

--- Physiotherapist.java
+++ Physiotherapist.java
@@ -8,13 +8,35 @@ public class Physiotherapist {
     private String[] expertise; // Array of expertise areas
     private List<Treatment> timetable;
 
-    // Constructor
     public Physiotherapist(int id, String name, String address, String phone, String[] expertise, List<Treatment> timetable) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.expertise = expertise;
+        this.timetable = timetable;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String[] getExpertise() { return expertise; }
+    public void setExpertise(String[] expertise) { this.expertise = expertise; }
+
+    public List<Treatment> getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(List<Treatment> timetable) {
         this.timetable = timetable;
     }
 }
