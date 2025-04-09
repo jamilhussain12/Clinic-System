b8385acb21184ee92de0fbdfe33ac7ff92ed7e63
jamilhussain12 <ajamilhussain9@gmail.com>
2025-04-10 00:24:36 +0500

    Update Treatment.java
    Getter/Setters

--- Treatment.java
+++ Treatment.java
@@ -1,12 +1,21 @@
 public class Treatment {
     private String name;
-    private String dateTime; // Using String for simplicity, can be replaced with LocalDateTime for better handling
+    private String dateTime;
     private Physiotherapist physiotherapist;
 
-    // Constructor
     public Treatment(String name, String dateTime, Physiotherapist physiotherapist) {
         this.name = name;
         this.dateTime = dateTime;
         this.physiotherapist = physiotherapist;
     }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDateTime() { return dateTime; }
+    public void setDateTime(String dateTime) { this.dateTime = dateTime; }
+
+    public Physiotherapist getPhysiotherapist() { return physiotherapist; }
+    public void setPhysiotherapist(Physiotherapist physiotherapist) { this.physiotherapist = physiotherapist; }
+
 }
