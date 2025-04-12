6b23a5b3fef3c8e8fd30eef366163448ebb1495d
jamilhussain12 <ajamilhussain9@gmail.com>
2025-04-12 22:51:51 +0500

    Update AppointmentController.java
    Added get patients and get bookings for main method

--- AppointmentController.java
+++ AppointmentController.java
@@ -1,12 +1,25 @@
-import model.*;
 import java.util.*;
 
 public class AppointmentController {
     private List<AppointmentBooking> bookings;
     private List<Patient> patients;
- 
+
     public AppointmentController() {
         bookings = new ArrayList<>();
         patients = new ArrayList<>();
     }
+
+    public void bookAppointment(AppointmentBooking booking) {
+        bookings.add(booking);
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public List<AppointmentBooking> getBookings() {
+        return bookings;
+    }
+
+
 }
